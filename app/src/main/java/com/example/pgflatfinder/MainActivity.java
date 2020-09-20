46bce3bbda11a84ae9052144d9c6d8b16c426124
Ankit Jha <ankitsakshi9800@gmail.com>
2020-09-20 15:52:35 +0530

    bottom added with frags

--- app/src/main/java/com/example/pgflatfinder/MainActivity.java
+++ app/src/main/java/com/example/pgflatfinder/MainActivity.java
@@ -14,7 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+
     private BubbleNavigationLinearView bubbleNavigation;
 
 
